f466065c296f5b8efb8c170b4d0f6e8f463001d3
Ivan Urra <ivan.urra.wd@gmail.com>
2021-10-21 18:23:01 +0200

    testing

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <p>Hello World!</p>;
+  return (
+    <div>
+      <p>Hello World!</p>
+      <button>
+        <Link href="/test">TEST</Link>
+      </button>
+    </div>
+  );
 }
---
+++ pages/test.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Test = () => {
+  return (
+    <div>
+      <p>Test</p>
+      <button>
+        <Link href="/">Index</Link>
+      </button>
+    </div>
+  );
+};
+
+export default Test;
