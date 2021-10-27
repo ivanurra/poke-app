f2ad748b2ac875eb203c3f9d1c22cae400809b37
Ivan Urra <ivan.urra.wd@gmail.com>
2021-10-27 07:04:59 +0200

    dynamic routes

---
+++ pages/calamansi/[id]/description.js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+const Desc = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <p>Description</p>
+    </div>
+  );
+};
+
+export default Desc;
