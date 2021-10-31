67e15b9c6352f915c27a8704fec89e1d26023585
Ivan Urra <ivan.urra.wd@gmail.com>
2021-10-31 20:25:30 +0100

    install bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
-import Link from "next/image";
+// import Link from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <p>Hello World!</p>
-      <button>
+      <p className="text-uppercase">Title</p>
+      <button className="btn btn-primary">
         <Link href="/test">Test</Link>
       </button>
-      <button>
+      <button className="btn btn-secondary">
         <Link href="/photos">Photos</Link>
       </button>
     </div>
