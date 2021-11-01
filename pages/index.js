f3fb90e4c38a3e6a58746e532eaeed5d1d26c80a
Ivan Urra <ivan.urra.wd@gmail.com>
2021-11-01 12:12:02 +0100

    fetch api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
+const Pokemon = ({ pokemon }) => {
+  return <li>{pokemon.name}</li>;
+};
+
+export default function Pokemones({ pokemones }) {
+  console.log(pokemones);
   return (
     <div>
-      <p className="text-uppercase">Title</p>
+      <p className="text-uppercase">POKEMONES</p>
+      <ul>
+        <li>
+          {pokemones.map((pokemon) => (
+            <Pokemon pokemon={pokemon} key={pokemon.name} />
+          ))}
+        </li>
+      </ul>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
+  const data = await response.json();
+
+  return {
+    props: { pokemones: data.results },
+  };
+};
