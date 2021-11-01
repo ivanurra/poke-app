1e397803939dd1d4d602b093f72b7d7582b20809
Ivan Urra <ivan.urra.wd@gmail.com>
2021-11-01 11:17:17 +0100

    fresh project

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
       <p className="text-uppercase">Title</p>
-      <button className="btn btn-primary">
-        <Link href="/test">Test</Link>
-      </button>
-      <button className="btn btn-secondary">
-        <Link href="/photos">Photos</Link>
-      </button>
-      <Image src="/coffee.jpg" alt="Coffee mug" width={400} height={400}/>
     </div>
   );
 }
